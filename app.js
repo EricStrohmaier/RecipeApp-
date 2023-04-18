4f255cd0babfd7028469adaa483cbe49b9797227
Eric Strohmaier <eric.strohmaier00@gmail.com!git config --global user.email eric.strohmaier00@gmail.comgit config --global user.email eric.strohmaier00@gmail.com!>
2023-04-18 14:15:33 +0200

    final changes completed app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express"); // A popular web framework for Node.js
 const mongoose = require("mongoose"); // A MongoDB object modeling tool designed to work in an asynchronous environment
 const bodyParser = require("body-parser"); // A middleware that parses incoming request bodies in a middleware before your handlers
 const Recipe = require("./models/recipeDataModel");
+const recipeDataModel = require("./models/recipeDataModel");
 
 // Initializing an Express application
 const app = express();
@@ -10,18 +11,48 @@ mongoose.connect("mongodb://127.0.0.1:27017/recipes", { useNewUrlParser : true} 
 
 app.set('view engine', 'ejs');
 
-
 app.use(express.urlencoded({extended: true}));
+app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
+randomRecipe =[];
 
 app.get("/", async (req, res) => {
-    const recipes = await Recipe.find()
 
-  res.render("index",{recipes: recipes});
+    const recipe = await Recipe.find({})
+  res.render("index",{recipe: recipe });
 });
+app.post("/", async (req, res) => {
+  const categoryInput = req.body.category;
+  let recipe;
+  let randomRecipe;
+  let query = req.body.searchBox;
+  let regex = new RegExp(query, 'i');
+  
+let dataDisplay = await Recipe.find({ "$or": [{title: { $regex: regex }}] });
+
+   if (categoryInput === "random") {
+    const count = await Recipe.countDocuments();
+    const randomIndex = Math.floor(Math.random() * count);
+    randomRecipe = await Recipe.findOne().skip(randomIndex);
+
+    res.render("randomIndex", { randomRecipe: randomRecipe });
+  } else {
+    recipe = await Recipe.find({ category: { $in: categoryInput } });
+    
+    if (recipe.length > 0) {
+      res.render("index", { recipe: recipe });
+    } else if (dataDisplay.length > 0) {
+      res.render("searchIndex", { dataDisplay: dataDisplay });
+    } else {
+      res.render("index", { recipe: [] });
+    }
+  }
+});
+
+
 
 
 app.listen(3000, function () {
--- models/recipeDataModel.js
+++ models/recipeDataModel.js
@@ -4,28 +4,46 @@ const cheerio = require("cheerio");
 
 const recipeDataSchema = new mongoose.Schema({
   title: String,
-  link: String
+  link: String,
+  category: [String]
 });
 const Recipe  = mongoose.model("Recipe", recipeDataSchema);
 
 const urls = Array.from({ length: 101 }, (v, i) => `https://pinchofyum.com/recipes/all/page/${i + 1}`);
+
+// Declare an empty array to store categories
+let categories = [];
 
 Promise.all(urls.map((url) => axios.get(url)))
   .then((responses) => {
     responses.forEach((response) => {
       const $ = cheerio.load(response.data);
 
-      $("article.category-all").each((i, element) => {
+      $("article").each(async (i, element) => {
         const link = $(element).find("a.block").attr("href");
-        const title = $(element).find("h3").text();
 
-        // Create a new recipe document and save it to the database
-        const recipe = new Recipe({ title: title, link: link });
-        recipe.save()
+        // Check if the recipe already exists in the database
+        const existingRecipe = await Recipe.findOne({ link: link });
+        if (existingRecipe) {
+          //console.log("existing")
+          return;
+        }
+
+        const category = $(element).attr("class").split(" ").filter(c => c.startsWith("category-")&& c !== 'category-sos-series'&& c !== 'category-soup' && c !== 'category-quinoa'&& c !== 'category-healthy-choices'&& c !== 'category-january-meal-planning-bootcamp'&& c !== 'category-casserole'&& c !== 'category-legume'&& c !== 'category-avocado' && c !== 'category-dairy-free' && c !== 'category-the-soup-series' && c !== 'category-holiday-series' && c !== 'category-meal-prep' && c !== "category-life" && c !== 'category-5-ingredients' && c !== "category-recipes" && c !== "category-all").map((c) => c.substring(9));
+        if(category == "" ) {
+          // Don't print empty category 
+        } else {
+          const title = $(element).find("h3").text();
+          // Create a new recipe document and save it to the database with all the categories
+          categories.push(category);
+          const recipe = new Recipe({ title: title, link: link, category: category });
+          recipe.save()
+        }    
       });
     });
   })
   .catch((err) => console.error(err));
-  
+
+
   module.exports = mongoose.model("Recipe", recipeDataSchema);
 
