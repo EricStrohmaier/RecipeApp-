9cf0537b56606713d66179e464446460480262bc
Eric Strohmaier <eric.strohmaier00@gmail.com!git config --global user.email eric.strohmaier00@gmail.comgit config --global user.email eric.strohmaier00@gmail.com!>
2023-06-01 14:52:16 +0200

    Fixing bugs

--- app.js
+++ app.js
@@ -1,60 +1,72 @@
-// Importing necessary packages
-const express = require("express"); // A popular web framework for Node.js
-const mongoose = require("mongoose"); // A MongoDB object modeling tool designed to work in an asynchronous environment
-const bodyParser = require("body-parser"); // A middleware that parses incoming request bodies in a middleware before your handlers
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const Recipe = require("./models/recipeDataModel");
-const recipeDataModel = require("./models/recipeDataModel");
+const path = require("path");
 
-// Initializing an Express application
 const app = express();
-mongoose.connect("mongodb://127.0.0.1:27017/recipes", { useNewUrlParser : true} ,{ useUnifiedTopology: true},{ useCreateIndex: true})
+
+mongoose.connect("mongodb://127.0.0.1:27017/recipes", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+   connectTimeoutMS: 30000, // Increased timeout value to 30 seconds
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((error) => {
+  console.error("Error connecting to MongoDB", error);
+});
 
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json())
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(express.static("public"));
-
-randomRecipe =[];
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", async (req, res) => {
-
-    const recipe = await Recipe.find({})
-  res.render("index",{recipe: recipe });
-});
-app.post("/", async (req, res) => {
-  const categoryInput = req.body.category;
-  let recipe;
-  let randomRecipe;
-  let query = req.body.searchBox;
-  let regex = new RegExp(query, 'i');
-  
-let dataDisplay = await Recipe.find({ "$or": [{title: { $regex: regex }}] });
-
-   if (categoryInput === "random") {
-    const count = await Recipe.countDocuments();
-    const randomIndex = Math.floor(Math.random() * count);
-    randomRecipe = await Recipe.findOne().skip(randomIndex);
-
-    res.render("randomIndex", { randomRecipe: randomRecipe });
-  } else {
-    recipe = await Recipe.find({ category: { $in: categoryInput } });
-    
-    if (recipe.length > 0) {
-      res.render("index", { recipe: recipe });
-    } else if (dataDisplay.length > 0) {
-      res.render("searchIndex", { dataDisplay: dataDisplay });
-    } else {
-      res.render("index", { recipe: [] });
-    }
+  try {
+    const recipes = await Recipe.find({});
+    res.render("index", { recipes });
+  } catch (error) {
+    console.error("Error fetching recipes", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
+app.post("/", async (req, res) => {
+  try {
+    const categoryInput = req.body.category;
+    const query = req.body.searchBox;
+    const regex = new RegExp(query, 'i');
 
+    let recipes;
 
+    if (categoryInput === "random") {
+      const count = await Recipe.countDocuments();
+      const randomIndex = Math.floor(Math.random() * count);
+      const randomRecipe = await Recipe.findOne().skip(randomIndex);
+      res.render("randomIndex", { randomRecipe });
+    } else {
+      if (categoryInput) {
+        recipes = await Recipe.find({ category: { $in: categoryInput } });
+      } else if (query) {
+        recipes = await Recipe.find({ title: { $regex: regex } });
+      } else {
+        recipes = [];
+      }
 
-app.listen(3000, function () {
-  console.log(`App listening on port 3000!`);
+      if (recipes.length > 0) {
+        res.render("index", { recipes });
+      } else {
+        res.render("index", { recipes: [] });
+      }
+    }
+  } catch (error) {
+    console.error("Error processing form submission", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(3001, function () {
+  console.log(`App listening on port 3001!`);
 });
